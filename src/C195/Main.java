c5fbd17a7cbabb5f5a9271bd9425770422bc7bb9
pbald20@wgu.edu <Sheba2424!>
2023-06-26 14:34:42 -0600

    first

--- src/C195/Main.java
+++ src/C195/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         JDBC.openConnection();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 875, 325);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
