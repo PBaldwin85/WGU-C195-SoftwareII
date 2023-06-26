75521df9ed5e2bf1208ac2d0f19c2611772104d6
pbald20@wgu.edu <Sheba2424!>
2023-06-26 12:23:47 -0600

    first

---
+++ src/sample/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+import helper.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JDBC.openConnection();
+
+
+        JDBC.closeConnection();
+    }
+
+
+}
