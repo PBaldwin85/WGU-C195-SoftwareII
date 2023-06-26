16013a18bee479bc44ffd855203ba6e88d059d76
pbald20@wgu.edu <Sheba2424!>
2023-06-26 14:08:27 -0600

    first

---
+++ src/C195/login.java
@@ -0,0 +1,11 @@
+package C195;
+
+import helper.JDBC;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+public class login {
+    public void exit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+}
