1bfb0abd86821e58ccad247d27b84e63cb490f28
pbald20@wgu.edu <Sheba2424!>
2023-07-03 01:32:48 -0600

    first

--- src/C195/Main.java
+++ src/C195/Main.java
@@ -18,9 +18,10 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/** Main application class used for starting the application.
+ * Calls functions which populate information from the database.
+ */
 public class Main extends Application {
-
-
 
     public void start(Stage stage) throws IOException, SQLException {
         JDBC.openConnection();
@@ -37,34 +38,34 @@ public class Main extends Application {
         stage.show();
     }
 
-
-
+    /** Checks to see if the user has logged in. */
     public static boolean loggedIn = false;
 
+    /** Initiates the appointment Id. */
     private static int appointmentId = 0;
 
+    /** Generates an appointment Id. */
     public static int generateAppointmentId() {
         appointmentId ++;
         return appointmentId;
     }
-
+    /** Gets an appointment Id. */
     public static int getAppointmentId() {
         return appointmentId;
     }
 
-
+    /** Initiates the customer Id */
     private static int customerId = 0;
-
+    /** Generates the custoemr Id */
     public static int generateCustomerId() {
         customerId ++;
         return customerId;
     }
-
+    /** Gets the customer Id */
     public static int getCustomerId() {
         return customerId;
     }
-
-
+    /** Closes the database connection */
     public void stop() {
         JDBC.closeConnection();
     }
